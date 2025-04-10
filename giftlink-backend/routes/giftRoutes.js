6ddb5597381ce40dc4ef727183681e2ea07d444f
Ricky <ricky@rschooltoday.com>
2025-04-10 20:05:46 +0700

    fix import db

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connectToDatabase = require('../db');
+const connectToDatabase = require('../models/db');
 const logger = require('../logger');
 
 router.get('/', async (req, res) => {
