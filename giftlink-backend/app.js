d4a145156196903cc0f3240cff46cd7f3a179990
Ricky <ricky@rschooltoday.com>
2025-04-10 21:42:03 +0700

    backend auth

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,7 +7,6 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -16,12 +15,13 @@ const port = 3060;
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
+.catch((e) => console.error('Failed to connect to DB', e));
 
 
 app.use(express.json());
 
 // Route files
+const authRoutes = require('./routes/authRoutes');
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
 
@@ -34,6 +34,7 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
+app.use('/api/auth', authRoutes);
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
 
